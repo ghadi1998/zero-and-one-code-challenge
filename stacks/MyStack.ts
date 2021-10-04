85d91a3b1ac155e084d0cb942ba5096910dee08f
unknown <mdallalghadi@gmail.com>
2021-10-04 08:58:47 -0700

    first commit

---
+++ stacks/MyStack.ts
@@ -0,0 +1,26 @@
+import * as sst from "@serverless-stack/resources";
+
+export default class MyStack extends sst.Stack {
+  constructor(scope: sst.App, id: string, props?: sst.StackProps) {
+    super(scope, id, props);
+
+    // Create a HTTP API
+    const api = new sst.Api(this, "Api", {
+      routes: {
+        "GET /": "src/lambda.handler",
+      },
+    });
+
+
+
+    const myWebApp = new sst.ReactStaticSite(this, "ReactSite", {
+      path: "frontend",
+    });
+
+
+    // Show the endpoint in the output
+    this.addOutputs({
+      "ApiEndpoint": api.url,
+    });
+  }
+}
