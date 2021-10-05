24d7e55947479cad664ab6f771826469114775fa
unknown <mdallalghadi@gmail.com>
2021-10-05 12:02:07 +0300

    react app linked

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,5 +1,6 @@
 import * as sst from "@serverless-stack/resources";
 
+
 export default class MyStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props);
@@ -13,13 +14,21 @@ export default class MyStack extends sst.Stack {
 
 
 
-    const myWebApp = new sst.ReactStaticSite(this, "ReactSite", {
-      path: "frontend",
-    });
+    const mySite = new sst.ReactStaticSite(this, "ReactSite", {
+       path: "frontend",
+       buildOutput: "build",
+       buildCommand: "npm run build",
+       errorPage: sst.StaticSiteErrorOptions.REDIRECT_TO_INDEX_PAGE,
+       environment: {
+        // Pass in the API endpoint to our app
+        REACT_APP_API_URL: api.url,
+      },
+     });
 
 
     // Show the endpoint in the output
     this.addOutputs({
+      Domain: mySite.distributionDomain,
       "ApiEndpoint": api.url,
     });
   }
