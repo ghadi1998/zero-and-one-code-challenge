c94e2ebf7e29cc61ac87c1ccc638161df0e29ebd
unknown <mdallalghadi@gmail.com>
2021-10-06 13:50:19 +0300

    changed amplify frontend

--- src/profile.ts
+++ src/profile.ts
@@ -1,23 +1,25 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyHandlerV2 } from "aws-lambda";
+import * as AWS from "aws-sdk"
+const dynamoDb = new AWS.DynamoDB()
+
 
 export const handler: APIGatewayProxyHandlerV2 = async (
     event: APIGatewayProxyEventV2
 ) => {
-
     try {
 
-        return {
-            statusCode: 200,
-            headers: { "Content-Type": "text/plain" },
-            body: event.body,
-        };
-    } catch (error) {
-        return {
-            statusCode: 200,
-            headers: { "Content-Type": "text/plain" },
-            body: JSON.stringify(error),
-        };
-    }
 
 
+      return {
+          statusCode: 200,
+          headers: { "Content-Type": "text/plain" },
+          body: event.body,
+      };
+  } catch (error) {
+      return {
+          statusCode: 200,
+          headers: { "Content-Type": "text/plain" },
+          body: JSON.stringify(error),
+      };
+  }
 };
